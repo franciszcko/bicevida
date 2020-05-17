8f99592a28e4515c70c3fab08fe8651c61eb5454
fnunez@cleveritgroup.com <fnunezblack@gmail.com>
2020-05-17 09:41:01 -0400

    fix test ant add new test

--- test/indexTest.js
+++ test/indexTest.js
@@ -3,17 +3,17 @@ const expect = require ('chai').expect;
 const app = require('../index.js');
 const request = require('supertest');
 const config = require('../config/config.js');
-
+var  price_policy = 0;
 describe ('calcular pólizas', () => {
     
     after(() => {
       console.log("finish test..........");
     })
 
-    contador = 0;
+ 
     describe('get endpoint calcular...', function()
     {        
-        it ('OK, Creando un request', function(done) 
+        it ('calculo de copago empleado y verificación de campos', function(done) 
         {
             request(app).get('/calcular').then((res) => 
             {                 
@@ -23,6 +23,7 @@ describe ('calcular pólizas', () => {
                 expect(outputJson).to.contain.property('calculo_nomina');
                 expect(outputJson).to.contain.property('costo_contrato_empresa');
                 var perc_employe = 100 - config.perc_company ;
+                
                 for (value in outputJson.detalle_empleados)
                 {
                    
@@ -30,34 +31,41 @@ describe ('calcular pólizas', () => {
                     expect(outputJson.detalle_empleados[value]).to.contain.property('hijos');  
                     expect(outputJson.detalle_empleados[value]).to.contain.property('copago_empleado');  
              
-                    if (outputJson.detalle_empleados[value] < 66 )
+                    if (outputJson.detalle_empleados[value].edad < 66 )
                     {
                                     
                         outputJson.detalle_empleados[value].copago_empleado 
+                       
                         if (outputJson.detalle_empleados[value].hijos < 1)
                         {
-                            var prima_employe = config.no_child * perc_employe / 100;                                    
+                            var prima_employe = config.no_child * perc_employe / 100;
+                            price_policy += config.no_child;
+                            console.log("costo poliza sin hijos: " + config.no_child);                                           
                             expect(outputJson.detalle_empleados[value].copago_empleado).to.contain.equals(prima_employe); 
                                     
                         }
                         if (outputJson.detalle_empleados[value].hijos == 1)
                         {
 
-                            var prima_employe = config.one_child * perc_employe / 100;                                    
+                            var prima_employe = config.one_child * perc_employe / 100; 
+                            price_policy += config.one_child;   
+                            console.log("costo poliza 1 hijo: " +config.one_child);                                 
                             expect(outputJson.detalle_empleados[value].copago_empleado).to.contain.equals(prima_employe); 
                                     
                         }
                         if (outputJson.detalle_empleados[value].hijos > 1)
                         {
 
-                            var prima_employe = config.two_childs * perc_employe / 100;                                    
+                            var prima_employe = config.two_childs * perc_employe / 100;  
+                            price_policy += config.two_childs;    
+                            console.log("costo poliza más de 1 hijo: " + config.two_childs);                                         
                             expect(outputJson.detalle_empleados[value].copago_empleado).to.contain.equals(prima_employe); 
                                     
                         }
                     }                   
-                }                             
-                        
-             done();
+                }
+                   
+                done();
 
             }).catch((err) => done(err));
                                
@@ -67,3 +75,26 @@ describe ('calcular pólizas', () => {
     });
 
 });
+
+describe('get endpoint calcular...', function()
+{        
+        it ('Calculo de costo de empresa', function(done) 
+        {
+            request(app).get('/calcular').then((res) => 
+            {                 
+              //  console.log(res);
+                var outputJson  = JSON.parse(res.text);
+                var costo = price_policy * config.perc_company/100;         
+                expect(outputJson.costo_contrato_empresa).to.contain.equals(costo)              
+                   
+                done();
+
+            }).catch((err) => done(err));
+                               
+
+        });          
+   
+
+});
+
+
