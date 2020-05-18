37f1f5aab2a36195bb3248c5e67e58d6bdb77050
fnunez@cleveritgroup.com <fnunezblack@gmail.com>
2020-05-18 08:31:30 -0400

    agregamos valores totalizados dentales y médicos

--- config/config.js
+++ config/config.js
@@ -1,9 +1,9 @@
 const config = {
   endpoint_policy: "https://dn8mlk7hdujby.cloudfront.net/interview/insurance/policy", 
   port:5011,
-  no_child: 0.279,
-  one_child: 0.4396,
-  two_childs: 0.5599, 
+  no_child: 0.399,
+  one_child: 0.6346,
+  two_childs: 0.8079, 
   perc_company: 90 
 }
 
